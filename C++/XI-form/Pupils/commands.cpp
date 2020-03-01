b6355a43187a595da05de38479728d26821529a5
Gaby <gitlanm@gmail.com>
2020-03-01 23:50:24 +0200

    Alphabetical order added (option 1)

--- C++/XI-form/Pupils/commands.cpp
+++ C++/XI-form/Pupils/commands.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
-#include <vector>
 #include "PupilModel.cpp"
 using namespace std;
 
-// cout << count << endl;
-
-int count;
-string FirstName[100], LastName[100];
+int st_count;
+string Pupil_Name[100], FirstName[100], LastName[100];
 
 // Option 1:
 void Show_List() {
   ifstream file;
-  string line, Pupil_Name;
-  int i = 0;
+  string line, word, Fullname[50], temp;
+  int n = 0, i = 1, j = 0;
 
   file.open("pupils_list.csv");
   while (file) {
     getline(file, line);
     istringstream ss(line);
-    while (getline(ss, Pupil_Name, ',')) {
-      if (i == 2) {
-        cout << endl;
-        i = 0;
-      }
-      cout << Pupil_Name;
+    while (getline(ss, word, ',')) {
+      Pupil_Name[i] = word;
+      if ((i % 2 == 0) && (j % 2 == 1)) {
+        n++;
+        PupilModel Pupil(Pupil_Name[j], Pupil_Name[i]);
+        Fullname[n] = Pupil.getFullname();
+      }      
       i++;
+      j++;
     }
-  }  
-  cout << endl;
+  }
+  
+  for (i = 2; i <= n; i++) {
+    for (j = 2; j <= n; j++) {
+      if(Fullname[j-1] > Fullname[j]) {
+        temp = Fullname[j-1];
+        Fullname[j-1] = Fullname[j];
+        Fullname[j] = temp;
+      }
+    }
+  }
+  
+  for (i = 1; i <= n; i++) {
+    cout << Fullname[i];
+    cout << endl;
+  }
   file.close();
 }
 
@@ -40,12 +52,14 @@ void Add_Pupil_Info_1() {
   fstream file;
 
   file.open("pupils_list.csv", ios::out | ios::app);
-  cout << "Enter First & Last name of the student: ";
-  cin >> First_Name >> Last_Name;
+  cout << "Enter First name of the student: ";
+  cin >> First_Name;
+  cout << "Enter Last name of the student: ";
+  cin >> Last_Name;
   PupilModel Pupil(First_Name, Last_Name);
   file << Pupil.getFName() << ", " << Pupil.getLName();
   file << "\n";
-  count++;
+  st_count++;
   file.close();
 }
 
@@ -57,11 +71,11 @@ void Add_Pupil_Info_2() {
   int i;
 
   file1.open("input_info.csv");
-  for (count = 0; getline(file1, line); count++);
+  for (st_count = 0; getline(file1, line); st_count++);
   file1.close();
 
   file1.open("input_info.csv");
-  while (!file1.eof() && i <= count) {
+  while (!file1.eof() && i <= st_count) {
     getline(file1, FirstName[i], ',');
     getline(file1, LastName[i]);
     file2.open("pupils_list.csv", ios::out | ios::app);
